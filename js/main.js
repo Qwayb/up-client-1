d65b0084ff0ecf217ee723100c6c0c8c646d4d0a
Углицкий Григорий Андреевич <gr424_uggan@ttit.local>
2025-02-04 12:38:43 +0700

    pract 10

--- js/main.js
+++ js/main.js
@@ -1,55 +1,148 @@
+Vue.component('product-review', {
+    template: `
+ 
+ <form class="review-form" @submit.prevent="onSubmit">
+ 
+ <p v-if="errors.length">
+  <b>Please correct the following error(s):</b>
+  <ul>
+    <li v-for="error in errors">{{ error }}</li>
+  </ul>
+ </p>
+ 
+  <p>
+    <label for="name">Name:</label>
+    <input id="name" v-model="name" placeholder="name">
+  </p>
+ 
+  <p>
+    <label for="review">Review:</label>
+    <textarea id="review" v-model="review"></textarea>
+  </p>
+ 
+  <p>
+    <label for="rating">Rating:</label>
+    <select id="rating" v-model.number="rating">
+      <option>5</option>
+      <option>4</option>
+      <option>3</option>
+      <option>2</option>
+      <option>1</option>
+    </select>
+  </p>
+
+  <p>
+    <label for="answer">Would you recommend this product?</label>
+    <label>
+      <input type="radio" id="yes" name="answer" value="yes" v-model="recommend"> Да
+    </label>
+    <label>
+      <input type="radio" id="no" name="answer" value="no" v-model="recommend"> Нет
+    </label>
+  </p>
+
+    <button type="submit">Отправить</button>
+ 
+ </form>
+  `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            recommend: null,
+            errors: []
+        }
+    },
+    methods:{
+        onSubmit() {
+            if(this.name && this.review && this.rating) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating,
+                    recommend: this.recommend
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+                this.rating = null
+                this.recommend = null
+            } else {
+                if(!this.name) this.errors.push("Name required.")
+                if(!this.review) this.errors.push("Review required.")
+                if(!this.rating) this.errors.push("Rating required.")
+                if(!this.recommend) this.errors.push("Recommend required")
+            }
+        }
+    }
+ })
+
 Vue.component('product', {
     template: `
     <div class="product">
-    <div class="product">
+        <div class="product">
     
-    <div class="product-image">
-        <img alt="#" :src="image" :alt="altText"/>
-    </div>
+            <div class="product-image">
+                <img alt="#" :src="image" :alt="altText"/>
+            </div>
 
-    <div class="product-info">
-        <h1>{{ title }}</h1>
-        <p>User is premium: {{ premium }}</p>
-        <p v-if="inStock == false" :class="{ lineTrough: !inStock || inventory == 0 }">Out of stock</p>
-        <p v-else-if="inventory > 10">In stock</p>
-        <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
-        <p v-else :class="{ lineTrough: !inStock || inventory == 0 }"> Out of stock </p>
-        <span v-show="onSale">{{ sale }}</span>
-        <ul>
-            <li v-for="detail in details">{{ detail }}</li>
-        </ul>
+        <div class="product-info">
+            <h1>{{ title }}</h1>
+            <p>User is premium: {{ premium }}</p>
+            <p v-if="inStock == false" :class="{ lineTrough: !inStock || inventory == 0 }">Out of stock</p>
+            <p v-else-if="inventory > 10">In stock</p>
+            <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
+            <p v-else :class="{ lineTrough: !inStock || inventory == 0 }"> Out of stock </p>
+            <span v-show="onSale">{{ sale }}</span>
+            <ul>
+                <li v-for="detail in details">{{ detail }}</li>
+            </ul>
 
-        <p>Shipping: {{ shipping }}</p>
+            <p>Shipping: {{ shipping }}</p>
 
-        <div
-        class="color-box"
-        v-for="(variant, index) in variants"
-        :key="variant.variantId"
-        :style="{ backgroundColor:variant.variantColor }"
-        @mouseover="updateProduct(index)"
-        ></div>
- 
+            <div
+            class="color-box"
+            v-for="(variant, index) in variants"
+            :key="variant.variantId"
+            :style="{ backgroundColor:variant.variantColor }"
+            @mouseover="updateProduct(index)"
+            ></div>
 
-        <div v-for="size in sizes">
-            <p> {{ size }} </p>
+            <div v-for="size in sizes">
+                <p> {{ size }} </p>
+            </div>
+
+            <button
+                    v-on:click="addToCart"
+                    :disabled="!inStock"
+                    :class="{ disabledButton: !inStock }"
+            >
+                Add to cart
+            </button>
+            
+            <button v-on:click="removeFromCart">Remove</button>
+                        
         </div>
-        <button
-                v-on:click="addToCart"
-                :disabled="!inStock"
-                :class="{ disabledButton: !inStock }"
-        >
-            Add to cart
-        </button>
-        <button v-on:click="removeFromCart">Remove</button>
-                      
-    </div>
 
+        <div class="more-products">
+            <a :href="link">More products like this</a>
+        </div>
 
-    <div class="more-products">
-        <a :href="link">More products like this</a>
-    </div>
-         
-</div>    </div>
+    </div>    
+</div>
+        <div>
+            <h2>Reviews</h2>
+            <p v-if="!reviews.length">There are no reviews yet.</p>
+            <ul>
+              <li v-for="review in reviews">
+              <p>{{ review.name }}</p>
+              <p>Rating: {{ review.rating }}</p>
+              <p>{{ review.review }}</p>
+              </li>
+            </ul>
+            </div> <product-review @review-submitted="addReview"></product-review>
+        </div>
     `,
 
     data() {
@@ -80,6 +173,7 @@ Vue.component('product', {
                 }
              ],         
             sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+            reviews: [],
         }
     },
 
@@ -97,6 +191,9 @@ Vue.component('product', {
             console.log(index);
         },    
 
+        addReview(productReview) {
+            this.reviews.push(productReview)
+        }
     },
 
     computed: {
